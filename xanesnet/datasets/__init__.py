2d6262c07ca74a239e1fbd2482576aa7b33dbfca
Tom Penfold <116175928+penfoldtj@users.noreply.github.com>
2026-05-26 15:38:54 +0100

    Delete xanesnet directory

--- xanesnet/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .graph import GraphDataset
-from .xanesx import XanesXDataset
-from .multihead import MultiheadDataset
-from .transformer import TransformerDataset
-from .envembed import EnvEmbedDataset
-from .e3eembed import E3EEmbdedDataset
--- xanesnet/descriptors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .rdc import RDC
-from .wacsf import WACSF
-from .pdos import PDOS
-from .direct import DIRECT
-from .mace import MACE
